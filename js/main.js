128205078e28cce90037f73417275a1916ac7fe1
Vladislav <themushket@gmail.com>
2021-10-29 13:15:48 +0300

    JS func

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('Hello world')
+"use strict";
+
+/* let userName = prompt( 'Введите логин:', '');
+
+ if (userName == 'Админ') {
+
+   let pass = prompt('Введите пароль', '');
+
+   if (pass =='Я главный') {
+    alert ('Здравствуйте!');
+   } else if ( pass == '' || pass == null ) {
+       alert('отменено');
+   } else {
+       alert('Неверный пароль')
+   }
+} else if (userName == '' || userName == null) {
+    alert('отменено')
+} else {
+    alert ('Я вас не знаю')
+}*/
+
+
+let num1 = +prompt('enter digit', '');
+let num2 = +prompt('enter digit', '');
+alert( num1 + num2 );
