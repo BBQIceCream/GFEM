85a90e81a81995f76eef028d433c1c83015a215f
Vlad <themushket@gmail.com>
2021-11-03 18:51:14 +0300

    calc switch

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
+/* const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели,', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько вы его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько вы его оцените?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); */
+
 "use strict";
 
-/* let userName = prompt( 'Введите логин:', '');
+let a = +prompt('Введите первое число', '');
+let b = +prompt('Введите второе число', '');
+let operator = prompt('Выберите оператор: + , - , / , * ', '');
+const isNotValid = !operator || isNaN(a) || isNaN(b) || typeof a !== 'number' || typeof b !== 'number';
 
- if (userName == 'Админ') {
+// Вызываем alert, внутрь передаем вызов функции calc()
+alert( calc(a, b, operator) );
 
-   let pass = prompt('Введите пароль', '');
+function calc(a, b, operator) {
+  
 
-   if (pass =='Я главный') {
-    alert ('Здравствуйте!');
-   } else if ( pass == '' || pass == null ) {
-       alert('отменено');
-   } else {
-       alert('Неверный пароль')
-   }
-} else if (userName == '' || userName == null) {
-    alert('отменено')
-} else {
-    alert ('Я вас не знаю')
-}*/
+  if (isNotValid) {
+      return 'error';
+  }
 
+  switch (operator) {
+      case '+':
+        return a + b;
 
-let num1 = +prompt('enter digit', '');
-let num2 = +prompt('enter digit', '');
-alert( num1 + num2 );
+      case '-':
+          return a - b;
+
+      case '*':
+          return a * b;
+
+      case '/':
+          return a / b;
+    
+       default:
+          alert('error');
+          break;
+  }
+}
